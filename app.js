c6a0bc8beff771b4443625abacca44564d3b4649
Aleksandr Vukolov <kolomel@yandex.ru>
2020-09-12 23:07:20 +0300

    Fix & refactoring routes Add readme

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require('express');
-const routes = require('./routes.js');
+const path = require('path');
+const routeCards = require('./routes/cards.js');
+const routeUsers = require('./routes/users.js');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
-app.use('/', routes);
+const errReqHandler = (req, res, next) => {
+  if (req.params.path !== 'users' && req.params.path !== 'cards') {
+    res.status('500').send({ message: 'Запрашиваемый ресурс не найден' });
+    return;
+  }
+  next();
+};
+
+app.use(express.static(path.join(__dirname, '../public')));
+app.use('/:path', errReqHandler);
+app.use('/', routeCards);
+app.use('/', routeUsers);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
---
+++ routes/cards.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const fs = require('fs').promises;
+const path = require('path');
+
+const pathToCards = path.join(__dirname, '../data/cards.json');
+router.get('/cards', (_req, res) => {
+  fs.readFile(pathToCards, 'utf8')
+    .then((cards) => {
+      res.send(JSON.parse(cards));
+    })
+    .catch(() => {
+      res.status('500').send({ message: 'Запрашиваемый ресурс не найден' });
+    });
+});
+
+module.exports = router;
