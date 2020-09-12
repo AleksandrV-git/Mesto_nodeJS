9edb7c0d3edcb96ca88b04b4bb7a836c3ae51d42
Aleksandr Vukolov <kolomel@yandex.ru>
2020-09-12 17:09:35 +0300

    add routes

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const routes = require('./routes.js');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
+app.use('/', routes);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
