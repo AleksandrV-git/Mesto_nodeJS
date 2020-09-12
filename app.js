6b1ccd1dac04cd4bd51a5964a8cbcc960ecc1ccf
Aleksandr Vukolov <kolomel@yandex.ru>
2020-09-12 10:28:41 +0300

    add public dir & express.static

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-//app.use(express.static(__dirname + '/public')); // теперь клиент имеет доступ только к публичным файлам
+app.use(express.static(__dirname + '/public'));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
