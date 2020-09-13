d38410ae2fde93c95b29d304509454937086681d
Aleksandr Vukolov <kolomel@yandex.ru>
2020-09-13 16:57:39 +0300

    Fix router in cards.js, add .eslintignore, fix eslint err

--- routes/cards.js
+++ routes/cards.js
@@ -3,7 +3,7 @@ const fs = require('fs').promises;
 const path = require('path');
 
 const pathToCards = path.join(__dirname, '../data/cards.json');
-router.get('/cards', (_req, res) => {
+router.get('/', (_req, res) => {
   fs.readFile(pathToCards, 'utf8')
     .then((cards) => {
       res.send(JSON.parse(cards));
